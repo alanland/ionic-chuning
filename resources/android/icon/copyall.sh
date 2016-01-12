61134ac8c2ac630382a2be89031ca668fa926293
alanland <alanland@163.com>
2016-01-12 16:34:28 +0800

    update icons

--- resources/android/icon/copyall.sh
+++ resources/android/icon/copyall.sh
@@ -1,7 +1,7 @@
 convert 1.jpg 1.png
-cp 1.jpg drawable-hdpi-icon.png
-cp 1.jpg drawable-ldpi-icon.png
-cp 1.jpg drawable-mdpi-icon.png
-cp 1.jpg drawable-xhdpi-icon.png
-cp 1.jpg drawable-xxhdpi-icon.png
-cp 1.jpg drawable-xxxhdpi-icon.png
+cp 1.png drawable-hdpi-icon.png
+cp 1.png drawable-ldpi-icon.png
+cp 1.png drawable-mdpi-icon.png
+cp 1.png drawable-xhdpi-icon.png
+cp 1.png drawable-xxhdpi-icon.png
+cp 1.png drawable-xxxhdpi-icon.png
--- scripts/build-install.sh
+++ scripts/build-install.sh
@@ -2,5 +2,5 @@
 
 
 ionic build
-adb -s 4TGETWCM75EQA6MV uninstall com.ionicframework.todo623409
+adb -s 4TGETWCM75EQA6MV uninstall com.github.alanland.chuning
 adb -s 4TGETWCM75EQA6MV install platforms/android/build/outputs/apk/android-debug.apk
